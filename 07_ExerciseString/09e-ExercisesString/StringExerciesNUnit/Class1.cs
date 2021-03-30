48f239368166d2f8386a73579e86945d768e61a8
JAlexCarney <JAlexCarney@gmail.com>
2021-03-30 17:53:52 -0500

    Created Exercise : Exercise String

---
+++ 07_ExerciseString/09e-ExercisesString/StringExerciesNUnit/Class1.cs
@@ -0,0 +1,133 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace StringExerciesNUnit
+{
+    public class StringExercises
+    {
+        /// <summary>
+        /// Given a string, return a "rotated left 2" version where the first 2 chars are moved to the end.
+        /// The string length will be at least 2.
+        // Given "Hello" return "lloHe"
+        // Given "java" return "vaja"
+        // Given "Hi" return "Hi"
+        /// </summary>
+        public string Left2(string input)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Given a string, return a formatted string with the following format. "Hello world, my name is {input}!"
+        // Given "Bob" return "Hello world, my name is Bob!")]
+        // Given ""  return "Hello world, my name is !")]
+        /// </summary>
+        public string StandardGreeting(string input)
+        {
+            throw new NotImplementedException();
+
+        }
+        /// <summary>
+        /// Given a string, return a string with words without vowels.
+        // Given "Hello" return "Hll"
+        // Given "GoodBye" return "GdB"
+        // Given "Euouae" return ""
+        /// </summary>
+        public string RemoveVowels(string input)
+        {
+
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Given 2 strings, a and b, return a string in the form long + short + long,
+        /// with the longer string on the outside and the shorter string on the inside.
+        /// The strings will not be the same length, but they may be empty (length 0).
+        // Given "Hello" and "hi" return "HellohiHello"
+        // Given "hi" and "Hello" return "HellohiHello"
+        // Given "aaa" and "b" return "aaabaaa"
+        /// </summary>
+        public string HiddenMessage(string inputA, string inputB)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        /// <summary>
+        /// Given a string, and a number, return a string from the x number of the end.
+        /// So "Candy", 4, returns "andy". X must be less then the length of the string.
+        /// String length must be greater than 1
+        // Given "Candy", and  4 return "andy"
+        // Given "and", and 2 return "nd"
+        // Given "coding", and 3 return "ing"
+        /// </summary>
+        public string SectionString(string input, int length)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    [TestFixture]
+    public class StringExercisesTest
+    {
+        StringExercises target = new StringExercises();
+        /// <summary>
+        /// Given a string, return a "rotated left 2" version where the first 2 chars are moved to the end.
+        /// The string length will be at least 2.
+        /// </summary>
+        [TestCase("Hello", "lloHe")]
+        [TestCase("java", "vaja")]
+        [TestCase("Hi", "Hi")]
+        public void Left2Test(string input, string expected)
+        {
+            Assert.AreEqual(target.Left2(input), expected);
+        }
+        /// <summary>
+        /// Given a string, return a formatted string with the following format. "Hello world, my name is {input}!"
+        /// </summary>
+        [TestCase("Bob", "Hello world, my name is Bob!")]
+        [TestCase("", "Hello world, my name is !")]
+        public void StandardGreetingTest(string input, string expected)
+        {
+            Assert.AreEqual(target.StandardGreeting(input), expected);
+
+        }
+        /// <summary>
+        /// Given a string, return a string with words without vowels.
+        /// </summary>
+        [TestCase("Hello", "Hll")]
+        [TestCase("GoodBye", "GdB")]
+        [TestCase("Euouae", "")]
+
+        public void RemoveVowelsTest(string input, string expected)
+        {
+            Assert.AreEqual(target.RemoveVowels(input), expected);
+
+        }
+        /// <summary>
+        /// Given 2 strings, a and b, return a string of the form long + short + long,
+        /// with the longer string on the outside and the shorter string on the inside.
+        /// The strings will not be the same length, but they may be empty (length 0).
+        /// </summary>
+        [TestCase("Hello", "hi", "HellohiHello")]
+        [TestCase("hi", "Hello", "HellohiHello")]
+        [TestCase("aaa", "b", "aaabaaa")]
+        public void HiddenMessageTest(string inputA, string inputB, string expected)
+        {
+            Assert.AreEqual(target.HiddenMessage(inputA, inputB), expected);
+
+        }
+
+        /// <summary>
+        /// Given a string, and a number, return a string from the x number of the end.
+        /// So "Candy", 4, returns "andy". X must be less then the length of the string.
+        /// String length must be greater than 1
+        /// </summary>
+        [TestCase("Candy", 4, "andy")]
+        [TestCase("and", 2, "nd")]
+        [TestCase("coding", 3, "ing")]
+        public void SectionStringTest(string input, int length, string expected)
+        {
+            Assert.AreEqual(target.SectionString(input, length), expected);
+
+        }
+    }
+}
---
+++ 08_MethodTestAndExercises/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+
+namespace _08_MethodTestAndExercises
+{
+    class Program
+    {
+        public static void WriteRainbow(string s) 
+        {
+            // create a RNG
+            Random r = new Random();
+            // loop through each char of s
+            foreach (char c in s) 
+            {
+                // pick a random number from 0 to 7
+                float n = r.Next(7);
+                
+                // use number to associate to color in ROYGBIV
+                if (n < 1)
+                    Console.ForegroundColor = ConsoleColor.Red;
+                else if (n < 2) 
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                else if (n < 3)
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                else if (n < 4)
+                    Console.ForegroundColor = ConsoleColor.Green;
+                else if (n < 5)
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                else if (n < 6)
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                else if (n < 7)
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.Write(c);
+            }
+
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        
+        static void Main(string[] args)
+        {
+            WriteRainbow("Hello, World!\n");
+        }
+    }
+}
