940c0a158e7348903813c988776d30aca9166f9a
JAlexCarney <JAlexCarney@gmail.com>
2021-05-13 15:59:31 -0500

    Babies First Migration!

---
+++ InvoiceSchemaMigration/InvoiceSchemaMigration/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using InvoiceSchemaMigration.Entities;
+
+namespace InvoiceSchemaMigration
+{
+    class AppDbContext : DbContext
+    {
+        public DbSet<Customer> Customer { get; set; }
+        public DbSet<Carrier> Carrier { get; set; }
+        public DbSet<Invoice> Invoice { get; set; }
+
+        public AppDbContext() : base()
+        {
+
+        }
+
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseSqlServer("Server=localhost;Database=AppDb;User Id=sa;Password=YOUR_strong_*pass4w0rd*");
+        }
+    }
+}
---
+++ InvoiceSchemaMigration/InvoiceSchemaMigration/Migrations/20210513204309_InitialCreate.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InvoiceSchemaMigration.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Carrier",
+                columns: table => new
+                {
+                    CarrierID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CarrierName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Carrier", x => x.CarrierID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Customer",
+                columns: table => new
+                {
+                    CustomerID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustomerName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customer", x => x.CustomerID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Invoice",
+                columns: table => new
+                {
+                    InvoiceID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustomerID = table.Column<int>(type: "int", nullable: false),
+                    CarrierID = table.Column<int>(type: "int", nullable: false),
+                    DueDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Total = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Invoice", x => x.InvoiceID);
+                    table.ForeignKey(
+                        name: "FK_Invoice_Carrier_CarrierID",
+                        column: x => x.CarrierID,
+                        principalTable: "Carrier",
+                        principalColumn: "CarrierID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Invoice_Customer_CustomerID",
+                        column: x => x.CustomerID,
+                        principalTable: "Customer",
+                        principalColumn: "CustomerID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invoice_CarrierID",
+                table: "Invoice",
+                column: "CarrierID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invoice_CustomerID",
+                table: "Invoice",
+                column: "CustomerID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Invoice");
+
+            migrationBuilder.DropTable(
+                name: "Carrier");
+
+            migrationBuilder.DropTable(
+                name: "Customer");
+        }
+    }
+}
